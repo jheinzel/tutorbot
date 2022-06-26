4e3e7b210de13455ea38321619a5e994f3af9920
Kistenxd <fabian.h.haas@gmx.at>
2022-06-26 11:40:52 +0200

    SubmissionsCommand.kt now supports retry, Refactored to be reusable from ReviewsCommand.kt

--- src/main/kotlin/at/fhooe/hagenberg/tutorbot/commands/SubmissionsCommand.kt
+++ src/main/kotlin/at/fhooe/hagenberg/tutorbot/commands/SubmissionsCommand.kt
@@ -1,19 +1,17 @@
 package at.fhooe.hagenberg.tutorbot.commands
 
+import at.fhooe.hagenberg.tutorbot.auth.MoodleAuthenticator
 import at.fhooe.hagenberg.tutorbot.components.BatchProcessor
 import at.fhooe.hagenberg.tutorbot.components.ConfigHandler
 import at.fhooe.hagenberg.tutorbot.network.MoodleClient
 import at.fhooe.hagenberg.tutorbot.components.PlagiarismChecker
 import at.fhooe.hagenberg.tutorbot.components.Unzipper
-import at.fhooe.hagenberg.tutorbot.util.exitWithError
-import at.fhooe.hagenberg.tutorbot.util.href
-import at.fhooe.hagenberg.tutorbot.util.promptBooleanInput
-import at.fhooe.hagenberg.tutorbot.util.promptTextInput
+import at.fhooe.hagenberg.tutorbot.util.*
 import picocli.CommandLine.Command
 import java.io.File
 import java.net.URI
-import java.nio.file.Path
 import javax.inject.Inject
+import kotlin.system.exitProcess
 
 @Command(
     name = "submissions",
@@ -24,16 +22,31 @@ class SubmissionsCommand @Inject constructor(
     private val unzipper: Unzipper,
     private val plagiarismChecker: PlagiarismChecker,
     private val batchProcessor: BatchProcessor,
-    private val configHandler: ConfigHandler
+    private val configHandler: ConfigHandler,
+    private val authenticator: MoodleAuthenticator
 ) : DownloadCommand(configHandler) {
 
     override fun execute() {
+        execute(null, null)
+    }
+
+    fun execute(assignmentUrl: String?, submissionOptions: Triple<Boolean, Boolean, Boolean>?){
+        authenticator.authenticate()
+
         val targetDirectory = setupTargetDirectory()
 
-        val assignmentUrl = promptTextInput("Enter assignment URL:")
-        val downloadLinks = getAllDownloadLinks(assignmentUrl)
-        if (downloadLinks.isEmpty()) {
-            exitWithError("Could not find any submissions to download")
+        val url = assignmentUrl ?: promptTextInput("Enter assignment URL:")
+
+        val (deleteArchives, checkPlagiarism, deleteSubmissions) = submissionOptions ?: promptForSubmissionOptions()
+
+
+        var downloadLinks = getAllDownloadLinks(url)
+        while (downloadLinks.isEmpty()) {
+            printlnRed("Could not find any submissions to download")
+            if(!promptBooleanInput("Try again? (Y/N)")){
+                exitProcess(0)
+            }
+            downloadLinks = getAllDownloadLinks(promptTextInput("Enter assignment URL:"))
         }
         val files = downloadLinks.map { link -> File(targetDirectory, getFileName(link)) }
 
@@ -45,24 +58,24 @@ class SubmissionsCommand @Inject constructor(
         }
 
         // Delete all archives if wanted
-        if (promptBooleanInput("Do you want to delete the extracted archives?")) {
+        if (deleteArchives) {
             files.forEach { archive -> archive.delete() }
         }
 
         // Check the results for plagiarism if wanted
-        if (promptBooleanInput("Do you want to check submissions for plagiarism?")) {
+        if (checkPlagiarism) {
             plagiarismChecker.generatePlagiarismReport(targetDirectory)
         }
 
         // Delete the downloaded submissions if wanted
-        if (promptBooleanInput("Do you want to delete the downloaded submissions again?")) {
+        if (deleteSubmissions) {
             files.map { zip -> File(zip.parent, zip.nameWithoutExtension) }.forEach { file ->
                 file.deleteRecursively()
             }
         }
     }
 
-    override fun getCommandSubDir(): String? {
+    override fun getCommandSubDir(): String {
         return configHandler.getSubmissionsSubDir() ?: promptTextInput("Enter submissions subdirectory:")
     }
 
@@ -82,4 +95,14 @@ class SubmissionsCommand @Inject constructor(
     private fun getFileName(link: String): String {
         return URI(link).path.split("/").last()
     }
+
+
 }
+
+fun promptForSubmissionOptions(): Triple<Boolean, Boolean, Boolean>{
+    val deleteArchives = promptBooleanInput("Do you want to delete the extracted archives?")
+    val checkPlagiarism = promptBooleanInput("Do you want to check submissions for plagiarism?")
+    val deleteSubmissions = promptBooleanInput("Do you want to delete the downloaded submissions again?")
+    return Triple(deleteArchives, checkPlagiarism, deleteSubmissions)
+}
+
