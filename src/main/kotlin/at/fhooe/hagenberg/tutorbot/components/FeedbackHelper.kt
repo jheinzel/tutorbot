325ad2704728fe25dbbcd9720307fe19444cf929
Marcel Salvenmoser <18032233+malthee@users.noreply.github.com>
2022-12-03 22:59:51 +0100

    Setup FeedbackHelper read file logic

--- src/main/kotlin/at/fhooe/hagenberg/tutorbot/components/FeedbackHelper.kt
+++ src/main/kotlin/at/fhooe/hagenberg/tutorbot/components/FeedbackHelper.kt
@@ -1,5 +1,9 @@
 package at.fhooe.hagenberg.tutorbot.components
 
+import at.fhooe.hagenberg.tutorbot.util.exitWithError
+import at.fhooe.hagenberg.tutorbot.util.promptTextInput
+import java.io.File
+import java.nio.file.Path
 import javax.inject.Inject
 
 class FeedbackHelper @Inject constructor(
@@ -7,10 +11,70 @@ class FeedbackHelper @Inject constructor(
 ) {
     // Tracks amount of feedbacks a student has received on submissions and reviews.
     data class FeedbackCount(val submission: Int, val review: Int)
+
     // Represents a Review, which has a student who submitted the code and one who reviewed it.
-    data class Review(val fileName: String, val studentSubNr: String, val studentRevNr: String) // TODO calculate sub and rev from fileName
+    data class Review(
+        val fileName: String,
+        val subStudentNr: String,
+        val revStudentNr: String
+    )
 
-    fun readFeedbackCountFromDir(): Map<String, FeedbackCount> = TODO("Read files from basedir/feedback.dir, sum up occurrences on left side as submitter, right side reviewer.")
+    /**
+     * Reads all review files from a directory matching with the STUDENT_NR_PATTERN ignoring other files.
+     */
+    private fun readAllReviewsFromDir(dir: File): List<Review> {
+        val studentNrRegex = STUDENT_NR_PATTERN.toRegex()
+        return dir.listFiles()?.mapNotNull { f ->
+            // Match first student number with submitter, second as reviewer
+            studentNrRegex.find(f.name)?.let { firstMatch ->
+                val submitter = firstMatch.value
+                firstMatch.next()?.let { secondMatch ->
+                    val reviewer = secondMatch.value
+                    Review(f.name, submitter, reviewer)
+                }
+            }
+        } ?: listOf()
+    }
 
-    fun readAvailableReviewsFromDir(): Collection<Review> = TODO("Read filenames from basedir/ex../review.dir")
+    /**
+     * Gathers the amount of feedbacks the students have received.
+     */
+    fun readFeedbackCountForStudents(): Map<String, FeedbackCount> {
+        val dirInput = configHandler.getFeedbackDir()
+            ?: promptTextInput("Enter directory with finished feedbacks (relative or absolute path):")
+        val feedbackDir = Path.of(dirInput).toFile()
+
+        if (!feedbackDir.isDirectory) {
+            exitWithError("Location $feedbackDir does not point to a valid directory.")
+        }
+
+        val reviews = readAllReviewsFromDir(feedbackDir)
+        val countMap = mutableMapOf<String, FeedbackCount>()
+        // Each occurrence as submitter increases the FeedbackCount.submissions, same for reviewer
+        // Respecting immutability of data class using copy
+        reviews.forEach { r ->
+            countMap[r.subStudentNr] = countMap[r.subStudentNr]?.let { it.copy(submission = it.submission + 1) }
+                ?: FeedbackCount(1, 0)
+            countMap[r.revStudentNr] = countMap[r.revStudentNr]?.let { it.copy(review = it.review + 1) }
+                ?: FeedbackCount(0, 1)
+        }
+
+        return countMap.toMap()
+    }
+
+    /**
+     * Gets all available reviews from an exercise.
+     */
+    fun readReviewsForExercise(): List<Review> {
+        val baseDir = configHandler.getBaseDir() ?: promptTextInput("Enter base directory:")
+        val exerciseSubDir = configHandler.getExerciseSubDir() ?: promptTextInput("Enter exercise subdirectory:")
+        val reviewsDir = configHandler.getReviewsSubDir() ?: promptTextInput("Enter reviews subdirectory:")
+        val targetDirectory = Path.of(baseDir, exerciseSubDir, reviewsDir).toFile()
+
+        return readAllReviewsFromDir(targetDirectory)
+    }
+
+    companion object {
+        const val STUDENT_NR_PATTERN = "[sS][0-9]+"
+    }
 }
