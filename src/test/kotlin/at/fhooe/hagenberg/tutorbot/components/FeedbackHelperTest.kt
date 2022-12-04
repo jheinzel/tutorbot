d68aa0615a342ccd969ff1e8351d2085267cbab0
Marcel Salvenmoser <18032233+malthee@users.noreply.github.com>
2022-12-04 17:17:17 +0100

    FeedbackHelper tests finished, file renamed with exercise prefix

--- src/test/kotlin/at/fhooe/hagenberg/tutorbot/components/FeedbackHelperTest.kt
+++ src/test/kotlin/at/fhooe/hagenberg/tutorbot/components/FeedbackHelperTest.kt
@@ -1,24 +1,22 @@
 package at.fhooe.hagenberg.tutorbot.components
 
-import at.fhooe.hagenberg.tutorbot.auth.MoodleAuthenticator
-import at.fhooe.hagenberg.tutorbot.commands.ReviewsCommand
-import at.fhooe.hagenberg.tutorbot.commands.SubmissionsCommand
-import at.fhooe.hagenberg.tutorbot.network.MoodleClient
+import at.fhooe.hagenberg.tutorbot.components.FeedbackHelper.FeedbackCount
 import at.fhooe.hagenberg.tutorbot.testutil.CommandLineTest
-import at.fhooe.hagenberg.tutorbot.testutil.getHtmlResource
-import at.fhooe.hagenberg.tutorbot.testutil.getResource
 import at.fhooe.hagenberg.tutorbot.testutil.rules.FileSystemRule
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.slot
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import java.io.File
-import java.nio.file.Path
 
 class FeedbackHelperTest : CommandLineTest() {
     private val lowerCaseStudentNumbers = listOf("s1", "s2", "s3", "s4", "s2210101010")
+    private val studentFeedbackCount = listOf(
+        "s1" to FeedbackCount(2, 0),
+        "s2" to FeedbackCount(0, 2),
+        "s3" to FeedbackCount(1, 0),
+        "s4" to FeedbackCount(1, 1),
+        "s2210101010" to FeedbackCount(0, 1)
+    )
 
     private val configHandler = mockk<ConfigHandler> {
         // Ex. and review dir is same as base
@@ -39,6 +37,7 @@ class FeedbackHelperTest : CommandLineTest() {
         val res = feedbackHelper.readReviewsForExercise()
         val invalidFile1 = "review.pdf"
         val invalidFile2 = "s1-invalid-file.pdf"
+
         assert(res.none { r -> r.fileName.contains(invalidFile1) || r.fileName.contains(invalidFile2) })
     }
 
@@ -53,21 +52,36 @@ class FeedbackHelperTest : CommandLineTest() {
     @Test
     fun `Read reviews includes right amount of files`() {
         val res = feedbackHelper.readReviewsForExercise()
+
         assert(res.size == 4)
     }
 
     @Test
-    fun `Read feedback gets student numbers lower case`() {
+    fun `Read reviews gets student numbers lower case`() {
         val res = feedbackHelper.readReviewsForExercise()
 
-        assert(res.all { r -> r.revStudentNr in lowerCaseStudentNumbers && r.subStudentNr in lowerCaseStudentNumbers})
+        assert(res.all { r -> r.revStudentNr in lowerCaseStudentNumbers && r.subStudentNr in lowerCaseStudentNumbers })
     }
 
     @Test
-    fun `Read feedback has entry for each student`() {
+    fun `Read feedback returns empty when dir empty`() {
         every { configHandler.getFeedbackDir() } returns fileSystem.directory.absolutePath
         val res = feedbackHelper.readFeedbackCountForStudents()
 
         assert(res.isEmpty())
+    }
+
+    @Test
+    fun `Read feedback has key for every lower case student number`() {
+        val res = feedbackHelper.readFeedbackCountForStudents()
+
+        assert(res.all { f -> f.key in lowerCaseStudentNumbers })
+    }
+
+    @Test
+    fun `Read feedback FeedbackCount has correct amount of submissions and reviews for each student`() {
+        val res = feedbackHelper.readFeedbackCountForStudents()
+
+        assert(studentFeedbackCount.all { s -> res[s.first] == s.second })
     }
 }
